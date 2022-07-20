b60144a03bf10d0ad1ecf05df47f912562d342b3
HammadSiddique <hammad.siddique01@gmail.com>
2022-07-20 15:55:48 +0500

    Create tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,3 +15,32 @@ CREATE TABLE "medical_histories" (
     FOREIGN KEY ("patient_id")
       REFERENCES "patients"("id")
 );
+
+CREATE TABLE "invoices" (
+  "id" INT,
+  "total_amount" DECIMA,
+  "generated_at" TIMESTAMP,
+  "payed_at" TIMEESTAMP,
+  "medical_history_id" INT,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "invoice_items" (
+  "id" INT,
+  "unit_price" DECIMAL,
+  "quantity" INT,
+  "total_price" DECIMAL,
+  "invoice_id" INT,
+  "treatment_id" INT,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "FK_invoice_items.invoice_id"
+    FOREIGN KEY ("invoice_id")
+      REFERENCES "invoices"("id")
+);
+
+CREATE TABLE "treatments" (
+  "id" INT,
+  "type" VARCHAR(100),
+  "name" VARCHAR(100),
+  PRIMARY KEY ("id")
+);
